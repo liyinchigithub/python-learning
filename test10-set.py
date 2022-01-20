2bb6153cbd834def6e0a0226abc9e56713ed646b
李银池 <liyinchi@qq.com>
2022-01-20 23:47:52 +0800

    集合 set

--- test10-set.py
+++ test10-set.py
@@ -4,11 +4,12 @@
 # Python 集合
 # https://www.runoob.com/python3/python3-set.html
 
-
+import pytest
 
 '''
 集合（set）是一个无序的不重复元素序列。
-可以使用大括号 { } 或者 set() 函数创建集合，注意：创建一个空集合必须用 set() 而不是 { }，因为 { } 是用来创建一个空字典。
+可以使用大括号 { } 或者 set() 函数创建集合
+注意：创建一个空集合必须用 set() 而不是 { }，因为 { } 是用来创建一个空字典。
 
 创建格式：
 parame = {value01,value02,...}
@@ -16,52 +17,97 @@ parame = {value01,value02,...}
 set(value)
 '''
 
+
+@pytest.mark.test
+def test_create_set():
+    set = {"a", "b", "c", "d"}  # 创建集合
+    print("set:{}".format(set))  # set:{'d', 'b', 'c', 'a'}
+
+    if "f" in set:  # 判断某个元素是否在集合中
+        print("在集合中")
+    else:
+        print("不再集合中")
+
+
 '''
 集合的基本操作
 1、添加元素
 '''
-def addSet():
-   thisset = set(("Google", "Runoob", "Taobao"))
-   thisset.add("Facebook")
-   print(thisset)
-# addSet()
+
+
+@pytest.mark.test
+def test_addSet():
+    thisset = set(("Google", "Runoob", "Taobao"))  # 创建集合 set()
+    thisset.add("Facebook")  # 添加元素到集合中
+    print(thisset)
+
 
 '''
-2、移除元素
+2、删除元素
 如果元素不存在，则会发生错误
 '''
-def delSet():
-   thisset = set(("Google", "Runoob", "Taobao"))
-   thisset.remove("Taobao")
-   print(thisset)
-# delSet()
+
+
+@pytest.mark.test
+def test_delSet():
+    thisset = set(("Google", "Runoob", "Taobao"))
+    thisset.remove("Taobao")
+    print(thisset)
+
 
 '''
-3、计算集合元素个数
+3、统计元素个数
 '''
-def countSet():
-   thisset = set(("Google", "Runoob", "Taobao"))
-   howLong=len(thisset)
-   print("howLong:",howLong)
-# countSet()
+
+
+@pytest.mark.test
+def test_countSet():
+    thisset = set(("Google", "Runoob", "Taobao"))
+    howLong = len(thisset)
+    print("howLong:", howLong)
+
 
 '''
 4、清空集合
 '''
-def clearSet():
-   thisset = set(("Google", "Runoob", "Taobao"))
-   thisset.clear()
-   print("thisset:",thisset) # 输出：set()
-# clearSet()
+
+
+@pytest.mark.test
+def test_clearSet():
+    thisset = set(("Google", "Runoob", "Taobao"))
+    thisset.clear()
+    print("thisset:", thisset)  # 输出：set()
+
 
 '''
 5、判断元素是否在集合中存在
 '''
-def isExisitSet():
-   thisset = set(("Google", "Runoob", "Taobao"))
-   isExisit="Runoob" in thisset
-   print("isExisit:",isExisit) # 输出：set()
-isExisitSet()
+
+
+@pytest.mark.test
+def test_isExisitSet():
+    thisset = set(("Google", "Runoob", "Taobao"))
+    isExisit = "Runoob" in thisset
+    print("isExisit:", isExisit)  # 输出：set()
+
+
+'''
+   - 差集
+   | 并集
+   & 交集
+   ^ 不同时存在
+'''
+
+
+@pytest.mark.test
+def test_():
+    a = set("adasdbcdef")
+    b = set('adafgsgda')
+    print(a)
+    print(a-b)# a和b 差集
+    print(a | b)# a和b 并集
+    print(a & b)# a和b 交集
+    print(a ^ b)# a和b 不同时存在的元素
 
 
 '''
@@ -85,4 +131,8 @@ symmetric_difference()	返回两个集合中不重复的元素集合。
 symmetric_difference_update()	移除当前集合中在另外一个指定集合相同的元素，并将另外一个指定集合中不同的元素插入到当前集合中。
 union()	返回两个集合的并集
 update()	给集合添加元素
+'''
+
+'''
+   命令行执行:pytest test10-set.py
 '''
