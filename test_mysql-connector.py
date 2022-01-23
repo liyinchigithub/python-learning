1a3e5c803cc6423093a1bad7cf613dba13b6c6ae
李银池 <liyinchi@qq.com>
2022-01-23 15:58:36 +0800

    mysql-connector

--- test_mysql-connector.py
+++ test_mysql-connector.py
@@ -42,30 +42,26 @@ except ConnectionError as e:
 '''
 cmd = conn.cursor()
 res = []
-@pytest.mark.skip
+@pytest.mark.test
 def test_select_fetchall():
     # 执行一条原生的SQL语句，执行结果保存在cmd中，没有返回值
-    cmd.execute("SELECT * FROM student")
+    sql="SELECT * FROM student"
+    cmd.execute(sql)
     # 可以使用fetchall()，获取所有的查询结果集，返回值为一个tuple，每一个元素是一个list
     global res
     res = cmd.fetchall()
     # 输出：[('jack', 21, 'quanzhou'), ('tom', 23, 'xiamen'), ('lili', 24, 'zhangzhou'), ('Haru', 23, 'fuzhou')]
-    print("内部res：{}".format(res))
-
-
-# test_select()
-print("外部res：{}".format(res))  # 输出：[]  注意：单元测试这边还是输出空列表，但非单元测试这边会输出和内部res一样的内容
+    print("test_select_fetchall res：{}".format(res))
 
 
 '''
     [执行select操作]
     使用fetchone()  每次只取一条记录
 '''
-
-
 @pytest.mark.skip
 def test_select_fetchone():
-    cmd.execute("SELECT * FROM student")
+    sql="SELECT * FROM student"
+    cmd.execute(sql)
     # 使用fetchone()返回一条结果集，每调用一次之后，内部指针会指向下一条结果集
     print(cmd.fetchone())  # ('jack', 21, 'quanzhou')
     print(cmd.fetchone())  # ('tom', 23, 'xiamen')
@@ -81,7 +77,8 @@ def test_select_fetchone():
 
 @pytest.mark.skip
 def test_select_fetchmany():
-    cmd.execute("SELECT * FROM student")
+    sql="SELECT * FROM student"
+    cmd.execute(sql)
     # 指定返回2条记录 输出：[('jack', 21, 'quanzhou'), ('tom', 23, 'xiamen')]
     res1 = cmd.fetchmany(2)
     print(res1)
@@ -109,10 +106,10 @@ def test_insert():
     [执行update操作]
     update操作，也都是使用execute方法，只需要将要执行的sql语句传入
 '''
-@pytest.mark.skip
+@pytest.mark.test
 def test_update():
     sql = "UPDATE student SET age = %s, address = %s WHERE id = %s"
-    val = (33, "beijing", 1)
+    val = (19, "wuhan", 1)
     cmd.execute(sql, val)
     conn.commit()  # [注意] 数据库修改后，需要使用mydb.commit()语句提交，不提交，修改不会生效。
     print(cmd.rowcount, " 条记录已更新")
@@ -122,8 +119,8 @@ def test_update():
     [执行delete操作]
     delete操作，也都是使用execute方法，只需要将要执行的sql语句传入
 '''
-@pytest.mark.skip
+@pytest.mark.test
 def test_delete():
-    res = cmd.execute("DELETE FROM student WHERE id={}".format(64932))
+    res = cmd.execute("DELETE FROM student WHERE name LIKE 'kk'".format(64932))
     conn.commit()  # [注意] 数据库修改后，需要使用mydb.commit()语句提交，不提交，修改不会生效。
     print(cmd.rowcount, " 条记录已更新")
