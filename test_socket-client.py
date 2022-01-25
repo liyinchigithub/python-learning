958b412ddafc2a04c651e402e516152a55a44f6b
李银池 <liyinchi@qq.com>
2022-01-25 16:05:26 +0800

    utils

--- test_socket-client.py
+++ test_socket-client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
-# 文件名：test-server-socket.py
-# socket 服务端
+# 文件名：test_client-socket.py
+# socket 客户端
 # https://www.runoob.com/python3/python3-socket.html
 
