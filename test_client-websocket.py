0b36adfb10f33f22b4fdd6dd9deffd6acf17f53b
李银池 <liyinchi@qq.com>
2022-01-25 16:37:45 +0800

    webSocket库

--- test_client-websocket.py
+++ test_client-websocket.py
@@ -1,5 +1,39 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
-# 文件名：test_websocket-client.py
+# 文件名：test_client-websocket.py
 # socket 客户端
-# 
+# https://www.cnblogs.com/lsdb/p/10949766.html
+# https://www.cnblogs.com/sincere-ye/p/14500200.html
+
+import asyncio
+import websockets
+
+# 向服务器端认证，用户名密码通过才能退出循环
+async def auth_system(websocket):
+    while True:
+        cred_text = input("please enter your username and password: ")
+        await websocket.send(cred_text)
+        response_str = await websocket.recv()
+        if "congratulation" in response_str:
+            return True
+
+# 向服务器端发送认证后的消息
+async def send_msg(websocket):
+    while True:
+        _text = input("please enter your context: ")
+        if _text == "exit":
+            print(f'you have enter "exit", goodbye')
+            await websocket.close(reason="user exit")
+            return False
+        await websocket.send(_text)
+        recv_text = await websocket.recv()
+        print(f"{recv_text}")
+
+# 客户端主逻辑
+async def main_logic():
+    async with websockets.connect('ws://10.10.6.91:5678') as websocket:
+        await auth_system(websocket)
+
+        await send_msg(websocket)
+
+asyncio.get_event_loop().run_until_complete(main_logic())
