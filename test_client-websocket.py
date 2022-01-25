5e7e52bc99c89d3dd5f5985239e27a4077eb7450
李银池 <liyinchi@qq.com>
2022-01-25 16:29:52 +0800

    更新README

---
+++ test_client-websocket.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+# 文件名：test_websocket-client.py
+# socket 客户端
+# 
---
+++ test_server-websocket.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+# 文件名：test_server-websocket.py
+# socket 服务端
+# 
+
