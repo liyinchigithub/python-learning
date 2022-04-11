3f32c01f5355bc0b535ff935d75c858d55db7757
李银池 <liyinchi@qq.com>
2022-04-11 15:50:14 +0800

    字典多种方式

--- test3-operator.py
+++ test3-operator.py
@@ -180,3 +180,34 @@ def memberOperator():
       print("3 - 变量 a 在给定的列表中 list 中")
    else:
       print("3 - 变量 a 不在给定的列表中 list 中")
+      
+   # 身份运算符用于比较两个对象的存储单元
+   def identityOperator():
+         '''
+         is	is 是判断两个标识符是不是引用自一个对象	x is y, 类似 id(x) == id(y) , 如果引用的是同一个对象则返回 True，否则返回 False
+         is not	is not 是判断两个标识符是不是引用自不同对象	x is not y ， 类似 id(x) != id(y)。如果引用的不是同一个对象则返回结果 True，否则返回 False。
+         '''
+         a = 20
+         b = 20
+         
+         if ( a is b ):
+            print ("1 - a 和 b 有相同的标识")
+         else:
+            print ("1 - a 和 b 没有相同的标识")
+         
+         if ( id(a) == id(b) ):
+            print ("2 - a 和 b 有相同的标识")
+         else:
+            print ("2 - a 和 b 没有相同的标识")
+         
+         # 修改变量 b 的值
+         b = 30
+         if ( a is b ):
+            print ("3 - a 和 b 有相同的标识")
+         else:
+            print ("3 - a 和 b 没有相同的标识")
+         
+         if ( a is not b ):
+            print ("4 - a 和 b 没有相同的标识")
+         else:
+            print ("4 - a 和 b 有相同的标识")
